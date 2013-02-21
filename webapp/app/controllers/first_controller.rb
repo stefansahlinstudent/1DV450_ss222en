a29717daf9934d6f65a0a50f66673b65350a775c
Stefan Sahlin <sahlin27@gmail.com>
2013-02-21 14:45:58 +0100

    hej

--- webapp/app/controllers/first_controller.rb
+++ webapp/app/controllers/first_controller.rb
@@ -29,10 +29,11 @@ class FirstController < ApplicationController
 	  user = User.find_by_email(params[:email])
 	  if user && user.authenticate(params[:password])
 		session[:user_id] = user.id
+		session[:loggedIn] = true
 		redirect_to root_url, :notice => "Logged in!"
 	  else
 		flash.now.alert = "Invalid email or password"
-		render "new"
+		render "index"
 	  end
 	end
 	
--- webapp/db/migrate/20130209183253_create_users.rb
+++ webapp/db/migrate/20130209183253_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
 	  t.string "first_name", :limit => 20
 	  t.string "last_name", :limit => 40
-	 # t.string "password", :limit => 40
+	  t.string "password", :limit => 40
 	  t.string "email", :default => "", :null => false
 	  t.string "password_digest"
 		  	
